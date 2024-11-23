dd5124bdc42db993a537736c11a80803d13e806d
Erik Suris <87949031+erikvsuris@users.noreply.github.com>
2024-11-23 19:11:58 -0300

    Updating library

--- library.pl
+++ library.pl
@@ -1,84 +1,117 @@
-% Base de Dados de Livros (ChatGPT gerou os livros)
 :- dynamic livro/4.
-:- dynamic lista_livros/2.
-
-% Base inicial
 livro('O Senhor dos Aneis', 'J.R.R. Tolkien', 1954, '978-3-16-148410-0').
 livro('1984', 'George Orwell', 1949, '978-0-452-28423-4').
 
+:- dynamic lista_livros/2.
 lista_livros('head', '978-3-16-148410-0'). % Início da lista
 lista_livros('978-3-16-148410-0', '978-0-452-28423-4').
 lista_livros('978-0-452-28423-4', 'nil'). % Fim da lista
 
-% Base de Dados de Membros
 :- dynamic membro/3.
-membro('Alice Ferreira', 2023456, 'Engenharia de Software').
-membro('Bruno Souza', 2023123, 'Ciência da Computação').
-membro('Carla Menezes', 2023890, 'Engenharia Elétrica').
-membro('Diego Martins', 2023674, 'Administração').
-membro('Eduarda Lima', 2023567, 'Psicologia').
-membro('Felipe Almeida', 2023987, 'Design Gráfico').
-membro('Gabriela Torres', 2023777, 'Engenharia Mecânica').
-membro('Henrique Lopes', 2023444, 'Medicina').
-membro('Isabela Santos', 2023666, 'Biomedicina').
-membro('João Pedro Rocha', 2023333, 'Direito').
-membro('Karina Oliveira', 2023555, 'Arquitetura e Urbanismo').
-membro('Lucas Andrade', 2023999, 'Física').
-membro('Mariana Silva', 2023222, 'Letras').
-membro('Nathalia Ribeiro', 2023111, 'Matemática').
-membro('Otávio Farias', 2023888, 'Engenharia Civil').
+membro('Alice Ferreira', '2023456', 'Engenharia de Software').
+membro('Bruno Souza', '2023123', 'Ciência da Computação').
+membro('Carla Menezes', '2023890', 'Engenharia Elétrica').
 
-% Base de Dados de Empréstimos
-:- dynamic emprestimo/3.
-emprestimo(2023456, '978-3-16-148410-0', '2024-11-01'). % Alice Ferreira
-emprestimo(2023123, '978-0-452-28423-4', '2024-11-03'). % Bruno Souza
-emprestimo(2023890, '978-0-19-953556-9', '2024-11-05'). % Carla Menezes
-emprestimo(2023674, '978-85-06-02528-7', '2024-11-07'). % Diego Martins
-emprestimo(2023567, '978-0-7475-3269-9', '2024-11-09'). % Eduarda Lima
-emprestimo(2023987, '978-0-385-50420-1', '2024-11-10'). % Felipe Almeida
-emprestimo(2023777, '978-1-5011-9762-5', '2024-11-11'). % Gabriela Torres
-emprestimo(2023444, '978-0-316-76948-0', '2024-11-12'). % Henrique Lopes
-emprestimo(2023666, '978-2-07-061275-8', '2024-11-13'). % Isabela Santos
-emprestimo(2023333, '978-0-06-088328-7', '2024-11-14'). % João Pedro Rocha
-emprestimo(2023555, '978-0-452-28424-1', '2024-11-15'). % Karina Oliveira
-emprestimo(2023999, '978-0-14-044913-6', '2024-11-16'). % Lucas Andrade
-emprestimo(2023222, '978-0-06-112241-5', '2024-11-17'). % Mariana Silva
-emprestimo(2023111, '978-0-14-143984-6', '2024-11-18'). % Nathalia Ribeiro
-emprestimo(2023888, '978-0-14-243720-9', '2024-11-19'). % Otávio Farias
-emprestimo(2023456, '978-3-16-148410-0', '2024-11-02'). % Alice Ferreira (repetindo livro em outra data)
-emprestimo(2023123, '978-0-7475-3269-9', '2024-11-06'). % Bruno Souza
-emprestimo(2023890, '978-2-07-061275-8', '2024-11-08'). % Carla Menezes
-emprestimo(2023674, '978-0-06-088328-7', '2024-11-09'). % Diego Martins
-emprestimo(2023555, '978-0-14-243720-9', '2024-11-20'). % Karina Oliveira (segundo empréstimo)
+:- dynamic lista_membros/2.
+lista_membros('head', '2023456').
+lista_membros('2023456', '2023123').
+lista_membros('2023123', '2023890').
+lista_membros('2023890', 'nil').
 
-% Cadastro de livro
+:- dynamic emprestimo/4.
+emprestimo('123', '2023456', '978-3-16-148410-0', '2024-11-01'). % Alice Ferreira
+emprestimo('221', '2023123', '978-0-452-28423-4', '2024-11-03'). % Bruno Souza
+emprestimo('314', '2023890', '978-0-19-953556-9', '2024-11-05'). % Carla Menezes
+
+:- dynamic lista_emprestimos/2.
+lista_emprestimos('head', '123').
+lista_emprestimos('123', '221').
+lista_emprestimos('221', '314').
+lista_emprestimos('314', 'nil').
+
 cadastrar_livro(Titulo, Autor, Ano, ISBN) :-
     assertz(livro(Titulo, Autor, Ano, ISBN)),
-    lista_livros(_, Tail),
-    retract(lista_livros(_, Tail)),
-    assertz(lista_livros(Tail, ISBN)),
+    lista_livros(Ultimo, 'nil'),
+    retract(lista_livros(Ultimo, 'nil')),
+    assertz(lista_livros(Ultimo, ISBN)),
     assertz(lista_livros(ISBN, 'nil')).
 
-% Listar livros
+cadastrar_membro(Nome, Matricula, Curso) :-
+    assertz(membro(Nome, Matricula, Curso)),
+    lista_membros(Ultimo, 'nil'),
+    retract(lista_membros(Ultimo, 'nil')),
+    assertz(lista_membros(Ultimo, Matricula)),
+    assertz(lista_membros(Matricula, 'nil')).
+
+emprestar_livro(ID, Matricula, ISBN, DataEmprestimo) :-
+    livro(_, _, _, ISBN),
+    membro(_, Matricula, _),
+	\+ emprestimo(ID, _, _, _),
+	assertz(emprestimo(ID, Matricula, ISBN, DataEmprestimo)),
+    lista_emprestimos(Ultimo, 'nil'),
+    retract(lista_emprestimos(Ultimo, 'nil')),
+    assertz(lista_emprestimos(Ultimo, ID)),
+    assertz(lista_emprestimos(ID, 'nil')).
+
 listar_livros :-
     lista_livros('head', Proximo),
     listar_livros_rec(Proximo).
 
-listar_livros_rec('nil') :- !. % Caso base
+listar_livros_rec('nil') :- !.
 listar_livros_rec(ISBN) :-
     livro(Titulo, Autor, Ano, ISBN),
     format("Título: ~w, Autor: ~w, Ano: ~w, ISBN: ~w~n", [Titulo, Autor, Ano, ISBN]),
     lista_livros(ISBN, Proximo),
     listar_livros_rec(Proximo).
 
-cadastrar_membro(Nome, Matricula, Curso) :-
-    assertz(membro(Nome, Matricula, Curso)).
+listar_membros :-
+    lista_membros('head', Proximo),
+    listar_membros_rec(Proximo).
 
-consultar_livro(Nome, Autor, Ano, ISBN) :-
-    livro(Nome, Autor, Ano, ISBN).
+listar_membros_rec('nil') :- !.
+listar_membros_rec(Matricula) :-
+    membro(Nome, Matricula, Curso),
+    format("Nome: ~w, Matrícula: ~w, Curso: ~w~n", [Nome, Matricula, Curso]),
+    lista_membros(Matricula, Proximo),
+    listar_membros_rec(Proximo).
 
-emprestar_livro(Matricula, ISBN, DataEmprestimo) :-
-  livro(_, _, _, ISBN),
-  membro(_, Matricula, _),
-  \+ emprestimo(Matricula, ISBN, _),
-  assertz(emprestimo(Matricula, ISBN, DataEmprestimo)).
+listar_emprestimos :-
+    lista_emprestimos('head', Proximo),
+    listar_emprestimos_rec(Proximo).
+
+listar_emprestimos_rec('nil') :- !.
+listar_emprestimos_rec(ID) :-
+    emprestimo(ID, Matricula, ISBN, DataEmprestimo),
+    format("ID: ~w, Matricula: ~w, ISBN: ~w, DataEmprestimo: ~w~n", [ID, Matricula, ISBN, DataEmprestimo]),
+    lista_emprestimos(ID, Proximo),
+    listar_emprestimos_rec(Proximo).
+
+consultar_livro_por_titulo(Titulo) :-
+    livro(Titulo, Autor, Ano, ISBN),
+    format("Título: ~w, Autor: ~w, Ano: ~w, ISBN: ~w~n", [Titulo, Autor, Ano, ISBN]).
+
+consultar_livro_por_autor(Autor) :-
+    livro(Titulo, Autor, Ano, ISBN),
+    format("Título: ~w, Autor: ~w, Ano: ~w, ISBN: ~w~n", [Titulo, Autor, Ano, ISBN]).
+
+consultar_livro_por_ano(Ano) :-
+    livro(Titulo, Autor, Ano, ISBN),
+    format("Título: ~w, Autor: ~w, Ano: ~w, ISBN: ~w~n", [Titulo, Autor, Ano, ISBN]).
+
+consultar_livro_por_isbn(ISBN) :-
+    livro(Titulo, Autor, Ano, ISBN),
+    format("Título: ~w, Autor: ~w, Ano: ~w, ISBN: ~w~n", [Titulo, Autor, Ano, ISBN]).
+
+consultar_livros_por_membro(Matricula) :-
+    emprestimo(_, Matricula, ISBN, DataEmprestimo),
+    livro(Titulo, Autor, Ano, ISBN),
+    format("Título: ~w, Autor: ~w, Ano: ~w, ISBN: ~w, Data de Empréstimo: ~w~n", [Titulo, Autor, Ano, ISBN, DataEmprestimo]).
+
+% relatorio_livros_disponiveis :-
+
+% relatorio_livros_emprestados :-
+    
+% 4. Consultas
+% Consultar livros: por título, por autor, por ano, emprestados a um
+% membro, listar todos os livros, listar todos os membros, relatório de
+% livros disponíveis, relatório de livros emprestados
